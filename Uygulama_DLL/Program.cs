5e9186bc72a6c341989c047902cfa5f6aba646e7
Osman Erdem Kılıç <osman_erdem1200@hotmail.com>
2022-02-08 13:59:06 +0300

    Add project files.

---
+++ Uygulama_DLL/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Matematiksel_Islemler;
+
+namespace Uygulama_DLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematiksel_Islemler.Islemler islemler = new Matematiksel_Islemler.Islemler();
+            Console.WriteLine(islemler.Topla(10, 7));
+
+            Console.ReadLine();
+        }
+    }
+}
